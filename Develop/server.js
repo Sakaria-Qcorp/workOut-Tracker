10dac5be2c88a93894c16b0b8515d9006fab5e2b
Zack <ahmedsakaria32@gmail.com>
2021-06-24 16:08:35 -0500

    correcting the variables

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true,
-    useCreateIndex: true 
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 
 });
 
