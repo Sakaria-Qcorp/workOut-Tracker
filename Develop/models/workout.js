49e7dc6e45ad5f1a28591cb1a0d22c301c47b32d
Zack <ahmedsakaria32@gmail.com>
2021-06-24 01:20:19 -0500

    working on the model

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,3 +3,44 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    day : {
+
+    },
+    exercises : [{
+        type: {
+
+        },
+        
+        name: {
+
+        },
+
+        duration : {
+
+        },
+
+        weight : {
+
+        },
+
+        reps : {
+
+        },
+
+        sets : {
+            
+        }
+
+    
+
+
+    }]
+
+
+
+    /* type: 'resistance',
+        name: 'Lateral Pull',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,*/
